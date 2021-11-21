c534649e97845dc46c59d563f8eb1c2b904d9fca
hatlonely <hatlonely@gmail.com>
2021-11-21 15:12:34 +0800

    update

--- flutter_article/lib/main.dart
+++ flutter_article/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter_article/pages/article/list_article_page.dart';
 
 void main() {
   runApp(const ArticleApp());
@@ -23,54 +24,10 @@ class ArticleApp extends StatelessWidget {
         appBarElevation: 5,
       ).toTheme,
       themeMode: ThemeMode.system,
-      // routes: HomePage.routes,
-      home: const HomePage(title: '程序员工具集'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      routes: {
+        '/tool/hash': (context) => ListArticlePage(),
+      },
+      home: ListArticlePage(),
     );
   }
 }
